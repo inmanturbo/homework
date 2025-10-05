6123038f8f0980abf3e51e24fd9b25cdcb0c3114
= <inman.turbo@gmail.com>
2025-10-05 14:05:19 -0400

    coding style

--- src/Concerns/SkipsAuthorizationForFirstPartyClients.php
+++ src/Concerns/SkipsAuthorizationForFirstPartyClients.php
@@ -8,11 +8,6 @@ trait SkipsAuthorizationForFirstPartyClients
 {
     /**
      * Determine if the client should skip the authorization prompt.
-     *
-     *
-     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
-     * @param  array  $scopes
-     * @return bool
      */
     public function skipsAuthorization(Authenticatable $user, array $scopes): bool
     {
